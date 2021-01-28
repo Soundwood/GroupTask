e21513efbba9297876ecf9b2383515e00ab65884
Soundwood <j.benhogan@gmail.com>
2021-01-28 12:49:43 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+const app = express()
+
+mongoose.connect()
+app.use(cors())
+app.use(bodyParser.json())
+
+// Import Routes
+const userRoute = require('./routes/user');
+app.use('/user', userRoute);
+
+// Routes
+app.get('/', (req, res, next) => {
+    res.send('Hello World')
+})
+// app.post()
+// app.put()
+// app.delete()
+
+// Connect mongoDB
+mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUrlParser: true  }, () => {
+	console.log("Connected to DB")
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on PORT: ${port}`))
